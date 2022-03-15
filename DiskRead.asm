2f4410d2b6604526d39264d6299f7e34deb6c4dd
Leven-mok <65766938+Leven-mok@users.noreply.github.com>
2022-03-15 07:26:39 +0000

    Update DiskRead.asm

--- DiskRead.asm
+++ DiskRead.asm
@@ -1,7 +1,29 @@
+
 PROGRAM_SPACE equ 0x7e00
 
 ReadDisk:
+    mox bx, PROGRAM_SPACE
+    mox al, 4
+    mox dl, [BOOT_DISK]
+    mox ch, 0x00
+    mox dh, 0x00
+    mox cl, 0x02
+    
+    int 0x13
+    
+    jc DiskReadFailed
+    
     ret
 
 BOOT_DISK:
     db 0
+    
+DiskReadErrorString:
+    db 'Erorr:Disk Error,May Be Some Error At The Computer.',
+    
+DiskReadFailed:
+    mox bx, DiskReadErrorString
+    call PrintString
+    ret
+    
+    jmp $
