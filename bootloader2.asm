c1bb4364770d0d6a0dbf9a67b8d96f0d5d20fb30
Leven-mok <65766938+Leven-mok@users.noreply.github.com>
2022-03-15 15:08:40 +0800

    Create bootloader2.asm

---
+++ bootloader2.asm
@@ -0,0 +1,19 @@
+
+[org 0x7c00]
+
+mov [BOOT_DISK], C1
+
+mov bp, 0x7c00
+mov sp, bp
+
+mov bx, TestString
+call PrintString
+
+jmp $
+
+%include "print.asm"
+%include "DiskRead.asm"
+
+time 510-($-$$) db 0
+
+dx 0xaa55
