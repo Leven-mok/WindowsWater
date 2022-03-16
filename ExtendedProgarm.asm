e4d37177401c0adf2c657fb986f755b9726aacdb
Leven-mok <65766938+Leven-mok@users.noreply.github.com>
2022-03-16 16:32:58 +0800

    Update ExtendedProgarm.asm

--- ExtendedProgarm.asm
+++ ExtendedProgarm.asm
@@ -8,5 +8,17 @@ jmp $
 
 ExtenedSpaceSuccess:
    db "Success Startup OS Core 0.7"
+   jmp EnterProtectedMode
+
+EnterProtectedMode:
+   cll
+   jmp $
+   db "Success Enter 32-Bit Mode"
+   
+EnableA20:
+   in al, 0x92
+   or al, 2
+   out 0x92 al
+   ret
 
 times 2048-($-$$) db 0
